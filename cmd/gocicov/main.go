bb1ba4783081ac1a867f30d981127cdfa6fa11fe
James Carter <james.carter@elasticpath.com>
2020-08-20 16:14:36 +0100

    Enforce coverage diff

--- cmd/gocicov/main.go
+++ cmd/gocicov/main.go
@@ -11,12 +11,20 @@ import (
 func main() {
 	forceTest := forcetest.New(".")
 	forceTest.Prepare()
-	defer forceTest.Cleanup()
-	coverage, err := coverage.Coverage()
+	cov, err := coverage.Get()
+	forceTest.Cleanup()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	fmt.Fprintf(os.Stderr, "Total coverage: %s\n", cov)
+	base := coverage.FromEnv("COVERAGE")
+	threshold := coverage.FromEnv("COVERAGE_THRESHOLD")
+	diff := cov - base
+	fmt.Fprintf(os.Stderr, "Coverage diff: %s\n", diff)
+	if threshold+diff < 0 {
+		fmt.Fprintf(os.Stderr, "Coverage diff failed to reach threshold of %s\n", threshold)
 		os.Exit(1)
 	}
-	fmt.Fprintf(os.Stderr, "Total coverage: %f\n", coverage)
-	fmt.Printf("%d\n", int(coverage*100))
+	fmt.Println(cov.Int())
 }
--- internal/coverage/coverage.go
+++ internal/coverage/coverage.go
@@ -1,60 +1,22 @@
 package coverage
 
 import (
-	"errors"
+	"fmt"
 	"os"
-	"os/exec"
 	"strconv"
-	"strings"
 )
 
-func runTests() error {
-	cmd := exec.Command("go", "test", "-v", "-coverprofile=c.out", "./...")
-	cmd.Stdout = os.Stderr
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+type Coverage float64
+
+func FromEnv(envVar string) Coverage {
+	f, _ := strconv.ParseFloat(os.Getenv(envVar), 64)
+	return Coverage(f / 100)
 }
 
-func gatherCoverage() (string, error) {
-	cmd := exec.Command("go", "tool", "cover", "-func=c.out")
-	data, err := cmd.Output()
-	return string(data), err
+func (c Coverage) String() string {
+	return fmt.Sprintf("%.2f%%", c)
 }
 
-func parseCoverage(coverage string) (float64, error) {
-	lines := strings.Split(coverage, "\n")
-	if len(lines) == 0 {
-		return 0, errors.New("no coverage data")
-	}
-	total := ""
-	for _, line := range lines {
-		if strings.HasPrefix(line, "total") {
-			total = line
-			break
-		}
-	}
-	if total == "" {
-		return 0, errors.New("no coverage data")
-	}
-
-	words := strings.Fields(total)
-	if len(words) < 3 {
-		return 0, errors.New("malformed coverage data")
-	}
-	percent := strings.TrimSuffix(words[len(words)-1], "%")
-
-	return strconv.ParseFloat(percent, 64)
-}
-
-func Coverage() (float64, error) {
-	defer os.Remove("c.out")
-	err := runTests()
-	if err != nil {
-		return 0, nil
-	}
-	coverage, err := gatherCoverage()
-	if err != nil {
-		return 0, nil
-	}
-	return parseCoverage(coverage)
+func (c Coverage) Int() int {
+	return int(c * 100)
 }
